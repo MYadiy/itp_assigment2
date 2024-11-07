b6ed3be7d1c94d9d591b79c06319a021af48fb31
21ftt1750 <99001373+21ftt1750@users.noreply.github.com>
2024-11-07 23:19:20 +0800

    Update Loan.class

--- src/App.java
+++ src/App.java
@@ -4,13 +4,13 @@ public class App {
     public static void main(String[] args) {
         Loan loan = new Loan(1, LocalDate.of(2024, 11, 1), LocalDate.of(2024, 11, 8), 202, 505, 404);
         
-        // Calculate cost for example
+        //ANI EXAMPLE SJA UNTUK CALCULATE COST. NANTI PAKAI SCANNER
         loan.costTotal(10.0, 30.0);
 
-        // Display loan details (for verification)
+        //ANI  UNTUK DISPLAY
         loan.displayLoanDetails();
         
-        // Save loan data to file
+        // ANI SAVE KE FILE LOAN.TXT NAMANYA
         loan.saveToFile("loan.txt");
     }
 }
