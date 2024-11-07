c324b81e399f0ed44051d8fdc3ee2bb06e4b1088
Yadiy <21ftt1747@student.pb.edu.bn>
2024-11-07 22:22:46 +0800

    update member

--- src/member.java
+++ src/member.java
@@ -1,5 +1,14 @@
 public class member {
+
     private String fName, lName, email, address, studentRoll;
     private int phoneNo;
 
+    public member (String fName, String lName, String email, String address, String studentRoll, int phoneNo){
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.address = address;
+        this.studentRoll = studentRoll;
+        this.phoneNo = phoneNo;
+    }
 }
