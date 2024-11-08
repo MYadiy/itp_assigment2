34cbfaa2ff27c4b07b78a55f7d3acf3ed9bfefe2
Yadiy <21ftt1747@student.pb.edu.bn>
2024-11-08 19:33:55 +0800

    mainMenu

---
+++ src/utbOutdoorsClub.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Member;
+import java.util.*;
+
+public class utbOutdoorsClub {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int memberCount = member.countLines("members.txt");
+        System.out.println("Loaded " + memberCount + " members");
+
+        System.out.println("\n");
+
+
+        do{
+            System.out.println("1. Add equipment \n2. Add equipment \n3. Loan equipment \n4. List loaned equipment \n5. List overdue equipment \n6. Return equipment \n7. Exit \n");
+            System.out.print("Choice: ");
+            int choice = input.nextInt();
+
+            if(0 <= choice && choice < 8){
+
+            }
+        }while(true);
+           
+
+
+       
+
+
+
+    }
+}
